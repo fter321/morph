d9c48d9ad15a94bdda3010b902db17c062777ed3
fter321 <hc9hc8hc9u@mail.com>
2020-10-28 17:19:59 +0300

    -

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame as pg
+import sys
+
+SIZE = WIDTH, HEIGHT = 640, 480
+speed_x, speed_y = 5, 5
+BLACK = (0, 0, 0)
+
+pg.init()
+pg.display.set_caption('Ball')
+screen = pg.display.set_mode(SIZE)
+
+ball = pg.image.load('ball.png')
+ball_rect = ball.get_rect()
+#print(ball_rect)
+
+while True:
+    for e in pg.event.get():
+        if e.type == pg.QUIT or \
+                e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE:
+            sys.exit(0)
+
+    ball_rect = ball_rect.move(speed_x, speed_y)      
+    if ball_rect.left < 0 or ball_rect.right > WIDTH:
+        speed_x = -speed_x
+    if ball_rect.top < 0 or ball_rect.bottom > HEIGHT:
+        speed_y = -speed_y
+
+    screen.fill(BLACK)
+    screen.blit(ball, ball_rect)
+    pg.display.update()
